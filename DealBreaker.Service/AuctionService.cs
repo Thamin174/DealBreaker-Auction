72da5c43cdd79bf811e2171090fafa1e1d70e576
Thamin174 <thaminnaik@gmail.com>
2020-08-02 11:16:35 +0530

    Crud operation performed in Auction

---
+++ DealBreaker.Service/AuctionService.cs
@@ -0,0 +1,53 @@
+﻿using DealBreaker.Entities;
+using DealDouble.Data;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DealBreaker.Service
+{
+    public class AuctionService
+    {
+        public Auction GetAuctionById(int Id)
+        {
+            DealBreakerContext context = new DealBreakerContext();
+
+            return context.Auctions.Where(a => a.Id == Id).First();
+        }
+        public List<Auction> GetAllAuction()
+        {
+            DealBreakerContext context = new DealBreakerContext();
+
+            return context.Auctions.ToList();
+        }
+        public void SaveAuctions(Auction auction)
+
+        {
+            DealBreakerContext context = new DealBreakerContext();
+
+            context.Auctions.Add(auction);
+            context.SaveChanges();
+        }
+
+        public void UpdateAuctions(Auction auction)
+
+        {
+            DealBreakerContext context = new DealBreakerContext();
+
+            context.Entry(auction).State = EntityState.Modified;
+            context.SaveChanges();
+        }
+
+        public void DeleteAuctions(Auction auction)
+
+        {
+            DealBreakerContext context = new DealBreakerContext();
+
+            context.Entry(auction).State = EntityState.Deleted;
+            context.SaveChanges();
+        }
+    }
+}
---
+++ DealBreaker.Web/Controllers/AuctionsController.cs
@@ -0,0 +1,61 @@
+﻿using DealBreaker.Entities;
+using DealBreaker.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DealBreaker.Web.Controllers
+{
+    public class AuctionsController : Controller
+    {
+        AuctionService auctionService = new AuctionService();
+        // GET: Auctions
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Listing()
+        {
+            var auction = auctionService.GetAllAuction();
+            return View(auction);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Auction auction)
+        {
+            auctionService.SaveAuctions(auction);
+            return RedirectToAction("Listing");
+        }
+
+        public ActionResult Edit(int Id)
+        {
+            var auction = auctionService.GetAuctionById(Id);
+            return View(auction);
+        }
+        [HttpPost]
+        public ActionResult Edit(Auction auction)
+        {
+            auctionService.UpdateAuctions(auction);
+            return RedirectToAction("Listing");
+        }
+
+        public ActionResult Delete(int Id)
+        {
+            var auction = auctionService.GetAuctionById(Id);
+            return View(auction);
+        }
+        [HttpPost]
+        public ActionResult Delete(Auction auction)
+        {
+            auctionService.DeleteAuctions(auction);
+            return RedirectToAction("Listing");
+        }
+    }
+}
