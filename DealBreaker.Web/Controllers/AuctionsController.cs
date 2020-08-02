7bfc782473f8116c765cd5cd8c6144715238b162
Thamin174 <thaminnaik@gmail.com>
2020-08-02 12:40:57 +0530

    Ajax function applied on Auction

--- DealBreaker.Web/Controllers/AuctionsController.cs
+++ DealBreaker.Web/Controllers/AuctionsController.cs
@@ -20,36 +20,30 @@ namespace DealBreaker.Web.Controllers
         public ActionResult Listing()
         {
             var auction = auctionService.GetAllAuction();
-            return View(auction);
+            return PartialView(auction);
         }
 
         public ActionResult Create()
         {
-            return View();
+            return PartialView();
         }
         [HttpPost]
         public ActionResult Create(Auction auction)
         {
             auctionService.SaveAuctions(auction);
-            return RedirectToAction("Listing");
+            return RedirectToAction("Index");
         }
 
         public ActionResult Edit(int Id)
         {
             var auction = auctionService.GetAuctionById(Id);
-            return View(auction);
+            return PartialView(auction);
         }
         [HttpPost]
         public ActionResult Edit(Auction auction)
         {
             auctionService.UpdateAuctions(auction);
-            return RedirectToAction("Listing");
-        }
-
-        public ActionResult Delete(int Id)
-        {
-            var auction = auctionService.GetAuctionById(Id);
-            return View(auction);
+            return RedirectToAction("Index");
         }
         [HttpPost]
         public ActionResult Delete(Auction auction)
